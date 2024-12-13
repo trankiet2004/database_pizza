96bfec71639336ba9aef264a1fcf7070dd963fe7
noobcoder123456789 <noobcoder1234567@gmail.com>
2024-12-13 19:42:24 +0700

    kéo API ca trực

--- Front_End/staff/js/post_ca_truc.js
+++ Front_End/staff/js/post_ca_truc.js
@@ -33,9 +33,9 @@ function generateWorkShift() {
 function getRandomDateAfterNextWeek() {
     const today = new Date();
     const nextWeekStart = new Date(today);
-    nextWeekStart.setDate(today.getDate() + (3 - today.getDay())); 
+    nextWeekStart.setDate(today.getDate() + (7 - today.getDay())); 
     const afterNextWeekStart = new Date(nextWeekStart);
-    afterNextWeekStart.setDate(afterNextWeekStart.getDate() + 3); 
+    afterNextWeekStart.setDate(afterNextWeekStart.getDate() + 7); 
     const afterNextWeekEnd = new Date(afterNextWeekStart);
     afterNextWeekEnd.setDate(afterNextWeekStart.getDate() + 30); 
     const randomDate = new Date(afterNextWeekStart.getTime() + Math.random() * (afterNextWeekEnd.getTime() - afterNextWeekStart.getTime()));
@@ -48,7 +48,7 @@ function getRandomDateAfterNextWeek() {
 try {
     fetch(
         `http://localhost:8000/shift/getShifts`, {
-            method: "GET"
+            method: "GET",
         }
     ).then(response => {
         if (!response.ok) {
@@ -71,13 +71,13 @@ try {
                 let day = date.getUTCDate();
                 let month = date.getUTCMonth() + 1;
                 let year = date.getUTCFullYear();
-                
+
                 listCaTrucTuanNayDiv += 
                 `<div style="width: 100%; height: auto; border: 1px solid black; border-radius: 10px; margin-bottom: 9px;">
                     <p style="font-size: 20px; font-weight: 600; line-height: 26px; color: black; margin: 11px;">
                         <nobr style="color: #DC802A;">GIỜ BẮT ĐẦU: </nobr>
 
-                        <nobr id="gio-bat-dau">${hourStart < 10 ? `0${hourStart}` : hourStart}:${minuteStart < 10 ? `0${minuteStart}` : minuteStart}</nobr>
+                    <nobr id="gio-bat-dau">${hourStart < 10 ? `0${hourStart}` : hourStart}:${minuteStart < 10 ? `0${minuteStart}` : minuteStart}</nobr>
 
                         <nobr style="font-size: 16px; font-style: italic; font-weight: 400; line-height: 20.8px; color: black; float: right;">
                             Số người thực hiện: ${res[i].currentparticipants}/${res[i].maxparticipants}
@@ -94,20 +94,23 @@ try {
                         <nobr style="color: #DC802A;">NGÀY THỰC HIỆN: </nobr>
 
                         <nobr id="gio-bat-dau">${day}/${month}/${year}</nobr>
+
+                        <button id="dang-ky-ca-truc" data-shift-id="${res[i].shiftid}" class="btn text-white" style="float: right; background-color: #49F057; font-size: 15px; font-weight: 700; line-height: 19.5px; width: 176px; height: 30px; border: 1px solid black; border-radius: 10px;">
+                            ĐĂNG KÝ
+                        </button>
                     </p>
                 </div>`;
             }
 
-            document.getElementById("list-ca-truc-tuan-nay").innerHTML = listCaTrucTuanNayDiv;
-            document.getElementById("so-ca-truc").innerHTML = res.length;
-            document.getElementById("so-ca-truc-can-dang-ky").innerHTML = `${getRandomInt(1, res.length)}/${getRandomInt(res.length, res.length + 5)}`;
+            document.getElementById("list-lua-chon-ca-truc").innerHTML = listCaTrucTuanNayDiv;
 
-            let listCaTrucTuanSauDiv = ``;
+            let listCaTrucDaChonDiv = ``;
 
-            for(let i = 0; i < getRandomInt(Math.max(res.length - 5, 1), res.length + 5); i ++) {
+            for(let i = 0; i < 4; i ++) {
                 const shifts = Math.random() < 0.5 ? generateWorkShift().morningShift : generateWorkShift().afternoonShift;
-                
-                listCaTrucTuanSauDiv += 
+                let MAX = getRandomInt(4, 8);
+
+                listCaTrucDaChonDiv += 
                 `<div style="width: 100%; height: auto; border: 1px solid black; border-radius: 10px; margin-bottom: 9px;">
                     <p style="font-size: 20px; font-weight: 600; line-height: 26px; color: black; margin: 11px;">
                         <nobr style="color: #DC802A;">GIỜ BẮT ĐẦU: </nobr>
@@ -115,7 +118,7 @@ try {
                         <nobr id="gio-bat-dau">${shifts.start}</nobr>
 
                         <nobr style="font-size: 16px; font-style: italic; font-weight: 400; line-height: 20.8px; color: black; float: right;">
-                            Số người thực hiện: ${getRandomInt(1, res.length)}/${getRandomInt(res.length, res.length + 5)}
+                            Số người thực hiện: ${getRandomInt(1, MAX)}/${MAX}
                         </nobr>
                     </p>
                     
@@ -129,13 +132,44 @@ try {
                         <nobr style="color: #DC802A;">NGÀY THỰC HIỆN: </nobr>
 
                         <nobr id="gio-bat-dau">${getRandomDateAfterNextWeek()}</nobr>
+
+                        <button class="btn text-white" style="float: right; background-color: #F48787; font-size: 15px; font-weight: 700; line-height: 19.5px; width: 176px; height: 30px; border: 1px solid black; border-radius: 10px;">
+                            HỦY ĐĂNG KÝ
+                        </button>
                     </p>
                 </div>`;
             }
 
-            document.getElementById("list-ca-truc-tuan-sau").innerHTML = listCaTrucTuanSauDiv;
+            document.getElementById("list-ca-truc-da-chon").innerHTML = listCaTrucDaChonDiv;
         });
     });
 } catch (error) {
     console.error(error.message);
 }
+
+document.getElementById("list-lua-chon-ca-truc").addEventListener("click", (event) => {    
+    if (event.target && event.target.id === "dang-ky-ca-truc") {        
+        const shiftId = event.target.getAttribute("data-shift-id");
+        
+        const postDangKyCaTruc = {
+            employee_id: "EMP00001", 
+            shift_id: shiftId
+        };
+
+        fetch(`http://localhost:8000/shift/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postDangKyCaTruc)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            alert("Đăng ký ca trực thành công!");
+            location.replace("http://localhost:8000/staff/ca_truc.html");
+        })
+        .catch(error => console.error(error));
+    }
+});
